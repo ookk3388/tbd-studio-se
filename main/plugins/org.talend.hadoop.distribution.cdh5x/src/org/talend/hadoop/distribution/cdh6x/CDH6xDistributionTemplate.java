c7a43a76d49e94e59470a2d4056b388e935363ba
slemoing <slemoing@talend.com>
2018-08-14 17:09:07 +0200

    feat(TBD-7497): Cloudera 6.0.0 integration

--- main/plugins/org.talend.hadoop.distribution.cdh5x/src/org/talend/hadoop/distribution/cdh6x/CDH6xDistributionTemplate.java
+++ main/plugins/org.talend.hadoop.distribution.cdh5x/src/org/talend/hadoop/distribution/cdh6x/CDH6xDistributionTemplate.java
@@ -13,14 +13,25 @@
 package org.talend.hadoop.distribution.cdh6x;
 
 import org.talend.hadoop.distribution.cdh5x.CDH5xDistributionTemplate;
+import org.talend.hadoop.distribution.component.HBaseComponent;
+import org.talend.hadoop.distribution.component.HCatalogComponent;
+import org.talend.hadoop.distribution.component.HDFSComponent;
+import org.talend.hadoop.distribution.component.HiveComponent;
+import org.talend.hadoop.distribution.component.HiveOnSparkComponent;
+import org.talend.hadoop.distribution.component.ImpalaComponent;
+import org.talend.hadoop.distribution.component.MRComponent;
+import org.talend.hadoop.distribution.component.PigComponent;
+import org.talend.hadoop.distribution.component.SqoopComponent;
 import org.talend.hadoop.distribution.dynamic.adapter.DynamicPluginAdapter;
+import org.talend.hadoop.distribution.dynamic.template.cdh.AbstractDynamicCDHDistributionTemplate;
 
 
 /**
  * DOC cmeng  class global comment. Detailled comment
  */
 @SuppressWarnings("nls")
-public class CDH6xDistributionTemplate extends CDH5xDistributionTemplate implements ICDH6xDistributionTemplate {
+public class CDH6xDistributionTemplate extends AbstractDynamicCDHDistributionTemplate implements HDFSComponent, HBaseComponent,
+HCatalogComponent, PigComponent, MRComponent, HiveComponent, ImpalaComponent, SqoopComponent, ICDH6xDistributionTemplate {
 
     public final static String TEMPLATE_ID = "CDH6xDistributionTemplate";
 
@@ -32,5 +43,100 @@ public class CDH6xDistributionTemplate extends CDH5xDistributionTemplate impleme
     public String getTemplateId() {
         return TEMPLATE_ID;
     }
+
+	@Override
+	public boolean doJavaAPISupportStorePasswordInFile() {
+		return true;
+	}
+
+	@Override
+	public boolean doJavaAPISqoopImportSupportDeleteTargetDir() {
+		return true;
+	}
+
+	@Override
+	public boolean doJavaAPISqoopImportAllTablesSupportExcludeTable() {
+		return true;
+	}
+
+	@Override
+	public boolean doSupportHive1() {
+		return false;
+	}
+
+	@Override
+	public boolean doSupportHive2() {
+		return true;
+	}
+
+	@Override
+	public boolean doSupportTezForHive() {
+		return false;
+	}
+
+	@Override
+	public boolean doSupportHBaseForHive() {
+		return false;
+	}
+
+	@Override
+	public boolean doSupportSSL() {
+		return true;
+	}
+
+	@Override
+	public boolean doSupportORCFormat() {
+		return true;
+	}
+
+	@Override
+	public boolean doSupportAvroFormat() {
+		return true;
+	}
+
+	@Override
+	public boolean doSupportParquetFormat() {
+		return true;
+	}
+
+	@Override
+	public boolean doSupportStoreAsParquet() {
+		return true;
+	}
+
+	@Override
+	public boolean doSupportCrossPlatformSubmission() {
+		return true;
+	}
+
+	@Override
+	public boolean doSupportImpersonation() {
+		return true;
+	}
+
+	@Override
+	public boolean doSupportHCatalog() {
+		return true;
+	}
+
+	@Override
+	public boolean doSupportHBase() {
+		return true;
+	}
+
+	@Override
+	public boolean pigVersionPriorTo_0_12() {
+		return false;
+	}
+
+	@Override
+	public boolean doSupportNewHBaseAPI() {
+		return true;
+	}
+
+	@Override
+	public boolean doSupportSequenceFileShortType() {
+		return true;
+	}
 
 }
